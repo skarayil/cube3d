d93f0af87e6e5582b359644e3a8eebafe45ac9fb
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2026-06-22 16:06:05 +0300

    creating count_lines and read_map functions

--- sources/parsing/read_file.c
+++ sources/parsing/read_file.c
@@ -6,11 +6,60 @@
 /*   By: skarayil <skarayil@student.42kocaeli>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 14:44:21 by skarayil          #+#    #+#             */
-/*   Updated: 2026/06/19 15:13:16 by skarayil         ###   ########.fr       */
+/*   Updated: 2026/06/22 16:04:50 by skarayil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char **read_map_file(char *file)
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static int	ft_count_lines(char *file)
 {
-    
+	int		fd;
+	int		count;
+	char	*line;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (-1);
+	count = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		count++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (count);
+}
+
+bool	ft_read_map(char *file, char ***lines)
+{
+	int		fd;
+	int		count;
+	int		i;
+	char	*line;
+
+	count = ft_count_lines(file);
+	if (count <= 0)
+		return (false);
+	*lines = malloc(sizeof(char *) * (count + 1));
+	if (!*lines)
+		return (false);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (false);
+	i = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		(*lines)[i++] = line;
+		line = get_next_line(fd);
+	}
+	(*lines)[i] = NULL;
+	close(fd);
+	return (true);
 }
