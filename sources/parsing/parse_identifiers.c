1022c9b8cc945adaa3ecc1886dae31365d43c65f
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2026-06-22 17:27:51 +0300

    creating texture_line funtion

--- sources/parsing/parse_identifiers.c
+++ sources/parsing/parse_identifiers.c
@@ -6,8 +6,31 @@
 /*   By: skarayil <skarayil@student.42kocaeli>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 16:11:31 by skarayil          #+#    #+#             */
-/*   Updated: 2026/06/22 16:11:56 by skarayil         ###   ########.fr       */
+/*   Updated: 2026/06/22 17:27:23 by skarayil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-cc -Wall -Wextra -Werror read_file.c get_next_line.c get_next_line_utils.c -o cub3D
+#include <stdbool.h>
+
+bool	ft_texture_line(char *line)
+{
+	if (!line)
+		return (false);
+	if ((line[0] == 'N') && (line[1] == 'O'))
+		return (true);
+	else if ((line[0] == 'S') && (line[1] == 'O'))
+		return (true);
+	else if ((line[0] == 'W') && (line[1] == 'E'))
+		return (true);
+	else if ((line[0] == 'E') && (line[1] == 'A'))
+		return (true);
+	return (false);
+}
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_texture_line("NO ./a.xpm"));
+	printf("%d\n", ft_texture_line("SO ./a.xpm"));
+	printf("%d\n", ft_texture_line("F 220,100,0"));
+	printf("%d\n", ft_texture_line("11111"));
+}
